e0464a35863d299f04ff1c042d5ff1e95f908469
Hristomira Sultova <hsultova@gmail.com>
2019-01-12 15:44:22 +0200

    Add main menu scene #4

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnContinueButtonClicked()
+    {
+
+    }
+
+    public void OnExitGameClicked()
+    {
+        Application.Quit();
+    }
+}
